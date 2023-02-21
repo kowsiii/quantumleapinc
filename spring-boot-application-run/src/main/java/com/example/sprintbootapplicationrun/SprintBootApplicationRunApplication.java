76e97fb7e758a6b09e7908202000702ca6497476
FieryCake <glenn-ham@hotmail.com>
2023-02-21 15:06:35 +0800

    adding in springboot

---
+++ spring-boot-application-run/src/main/java/com/example/sprintbootapplicationrun/SprintBootApplicationRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootapplicationrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootApplicationRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootApplicationRunApplication.class, args);
+	}
+
+}
---
+++ spring-boot-application-run/src/test/java/com/example/sprintbootapplicationrun/SprintBootApplicationRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootapplicationrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootApplicationRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
