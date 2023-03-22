dff7d3badb12de766da46b65072e2cd00ae46c99
FieryCake <glenn-ham@hotmail.com>
2023-03-22 20:00:25 +0800

    code cleanup

--- spring-boot-application-run/src/main/java/com/example/service/VendorService.java
+++ spring-boot-application-run/src/main/java/com/example/service/VendorService.java
@@ -25,43 +25,5 @@ public class VendorService {
         return repository.save(e);
     }
 
-    // public List<User> getEmployees(){
-    //     return repository.findAll();
-    // }
-
-    // public User getEmployeeById(int id){
-    //     return repository.findById(id).get();
-    // }
-
-    // public User validateUser(String email, String password){
-    //     User e= repository.findByEmail(email);
-    //     if(e.getPassword().equals(password)){
-    //         return e;
-    //     } else{
-    //         System.out.println(e.getPassword());
-    //         System.out.println(password);
-    //         return null;
-    //     }
-    // }
-
-//     public String deleteEmployee(int id){
-//         // try{
-//         //     repository.deleteById(id);
-//         // }catch(Exception e){
-//         //     return "No Such Employee";
-//         // }
-        
-//         // return "Employee deleted";
-//         Optional<User> e = repository.findById(id);
-//         System.out.println(e);
-//         if(ObjectUtils.isEmpty(e)){
-//             returnMsg m = new returnMsg(400,"error");
-//             return new Gson().toJson(m);
-//         }
-//         returnMsg m = new returnMsg(200,"no error");
-//         //add delete statement
-//         return new Gson().toJson(m);
-//     }
-// }
 }
 //entity -> repo -> service -> controller
