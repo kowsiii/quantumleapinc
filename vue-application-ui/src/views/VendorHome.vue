49eb48fd8a1a21c501a2585b6d4649ee31c79363
yimunnn <yimun.yip.2020@scis.smu.edu.sg>
2023-03-24 15:03:48 +0800

    Navbar for admin and vendor

--- vue-application-ui/src/views/VendorHome.vue
+++ vue-application-ui/src/views/VendorHome.vue
@@ -1,21 +1,24 @@
 <template>
+  <div>
+    <VendorNavBar />
+    <router-view />
     <div class="col-md-6">
       <div class="card card-container">
         Vendor!!
       </div>
     </div>
+  </div>
   </template>
   
   <script>
+  import VendorNavBar from '../components/VendorNavBar.vue';
+
   export default {
-    name: "VendorHome",
-    data () {
-        return {
-  
-        }},
-    methods: {
-      
-    }
+    name: "Vendorhome",
+    components: {
+      VendorNavBar,
+    },
+   
   
   }
   
