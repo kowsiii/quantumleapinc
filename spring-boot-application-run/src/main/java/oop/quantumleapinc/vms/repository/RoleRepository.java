8ca2b9e49b5a86faf885b46bf8a9914b6d169e63
FieryCake <glenn-ham@hotmail.com>
2023-04-01 16:55:52 +0800

    cleaned up code

--- spring-boot-application-run/src/main/java/oop/quantumleapinc/vms/repository/RoleRepository.java
+++ spring-boot-application-run/src/main/java/oop/quantumleapinc/vms/repository/RoleRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
+
+    boolean existsByName(ERole name);
 }
