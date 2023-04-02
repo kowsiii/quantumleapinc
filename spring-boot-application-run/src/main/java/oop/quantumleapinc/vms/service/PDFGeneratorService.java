11b0818f9429f108f2c327b792c34490fecedb09
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-04-02 15:27:50 +0800

    pdf generator updates (html input)

--- spring-boot-application-run/src/main/java/oop/quantumleapinc/vms/service/PDFGeneratorService.java
+++ spring-boot-application-run/src/main/java/oop/quantumleapinc/vms/service/PDFGeneratorService.java
@@ -1,40 +1,33 @@
 package oop.quantumleapinc.vms.service;
 
-import com.lowagie.text.*;
-import com.lowagie.text.pdf.PdfWriter;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StringReader;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+
+
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
 
 @Service
 public class PDFGeneratorService {
-    public void export(HttpServletResponse response) throws IOException {
+    public void export(HttpServletResponse response, String html) throws IOException,  DocumentException {
 
-        String html = "<h1>HTML to PDF</h1><p>This is an example of HTML to PDF conversion using iText.</p>";
+        // String html = "<h1>HTML to PDF</h1><p>This is an example of HTML to PDF conversion using iText.</p> <ol> <li>First item</li> <li>Second item</li> <li>Third item</li> </ol> <p>Here is an image: <img src=\"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png\" alt=\"Google logo\" /> </p> <p>Here is a link to <a href=\"https://www.google.com/\">Google</a>.</p> "  ;
         Document document = new Document(PageSize.A4);
         OutputStream out = response.getOutputStream();
-        PdfWriter.getInstance(document, out);
+        PdfWriter writer = PdfWriter.getInstance(document, out);
 
         document.open();
-        Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-        fontTitle.setSize(18);
 
-        Paragraph paragraph = new Paragraph("This is a title.", fontTitle);
-        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document, new StringReader(html));
 
-        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
-        fontParagraph.setSize(12);
-
-        Paragraph paragraph2 = new Paragraph("testing pdf", fontParagraph);
-        paragraph2.setAlignment(Paragraph.ALIGN_LEFT);
-
-        document.add(paragraph);
-        document.add(paragraph2);
-
-
-        
         document.close();
         out.flush();
     }
